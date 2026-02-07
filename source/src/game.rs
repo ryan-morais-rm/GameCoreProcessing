5690d88614563e984495fc14093febe62178ef4d
Ryan de Morais Correia <ryan.morais.workspace@gmail.com>
2026-02-07 16:46:08 -0300

    Implementing the inital design for Game Struct

---
+++ source/src/game.rs
@@ -0,0 +1,44 @@
+use std::fmt;
+use super::common_traits::data::Metadata;
+
+pub struct Game {
+    name: String, 
+    developer: String,
+    producer: String, 
+    genre: String, 
+    system: String,
+    date: String, 
+}
+
+impl fmt::Display for Game {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Name: {}, Genre: {}, System: {}, Date: {}", self.name, self.genre, self.system, self.date)
+    }
+}
+
+impl Metadata for Game {
+    fn common(&self) -> String {
+        format!("name: {}, date: {}", self.name, self.date)
+    }
+    fn specific(&self) -> String {
+        format!("producer: {}, developer: {}", self.producer, self.developer)
+    }
+}
+
+impl Game {
+    pub fn new(name: String, developer: String, producer:String, genre: String, system: String, date: String,) -> Self {
+        Self { name, developer, producer, genre, system, date }
+    }
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+    pub fn genre(&self) -> &str {
+        &self.genre 
+    }
+    pub fn system(&self) -> &str {
+        &self.system
+    }
+    pub fn date(&self) -> &str {
+        &self.date
+    }
+}
