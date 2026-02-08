f5cd8286b88ed391ff71c45b9e5ad8bbda6a64af
Ryan de Morais Correia <ryan.morais.workspace@gmail.com>
2026-02-08 17:54:02 -0300

    removed Result<> from select_options, removed Result<, String> from_str, function input_option added

--- source/src/menu.rs
+++ source/src/menu.rs
@@ -1,20 +1,4 @@
-#[derive(Debug, PartialEq)] 
-pub enum OptionMenu {
-    Q, 
-    S, 
-    L, 
-    E, 
-}
-
-pub fn from_str(s: &String) -> Result<OptionMenu, String> {
-    match s.to_uppercase().trim() {
-        "Q" => Ok(OptionMenu::Q),
-        "S" => Ok(OptionMenu::S),
-        "L" => Ok(OptionMenu::L),
-        "E" => Ok(OptionMenu::E),
-        _ => Err(format!("The option '{}' does not exist!", s)),
-    }
-}
+use std::io;
 
 pub fn show_menu() {
     println!("=========== MENU ==========="); 
@@ -24,23 +8,47 @@ pub fn show_menu() {
     println!("(e) exit");  
 }
 
-pub fn select_options(option: &OptionMenu) -> Result<bool, String> {
+pub fn input_option() -> String {
+    let mut option = String::new(); 
+    io::stdin().read_line(&mut option).expect("Option was not received");
+    option
+}
+
+#[derive(Debug, PartialEq)] 
+pub enum OptionMenu {
+    Q, 
+    S, 
+    L, 
+    E, 
+}
+
+pub fn from_str(s: &String) -> Result<OptionMenu, ()> {
+    match s.to_uppercase().trim() {
+        "Q" => Ok(OptionMenu::Q),
+        "S" => Ok(OptionMenu::S),
+        "L" => Ok(OptionMenu::L),
+        "E" => Ok(OptionMenu::E),
+        _ => Err(()),
+    }
+}
+
+pub fn select_options(option: &OptionMenu) -> bool {
     match option {
         OptionMenu::Q => { 
             println!("Processing quantity...");
-            Ok(false)
+            false
         }
         OptionMenu::S => {
             println!("Searching for games...");
-            Ok(false)
+            false
         }
         OptionMenu::L => {
             println!("Listing games...");
-            Ok(false)
+            false
         }
         OptionMenu::E => {
-            println!("Leaving...");
-            Ok(true)
+            println!("Leaving program...");
+            true
         }
     }
 }
