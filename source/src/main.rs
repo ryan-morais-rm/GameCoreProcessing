3fa73cb47db3a1be2e60077e41b6431183d5eef3
Ryan de Morais Correia <ryan.morais.workspace@gmail.com>
2026-02-08 23:54:16 -0300

    Initiating data modeling flow

--- source/src/main.rs
+++ source/src/main.rs
@@ -1,21 +1,33 @@
 mod menu;
 mod game;
 mod common_traits;
-use crate::menu::{from_str, select_options, show_menu, input_option};
+use crate::menu::{from_str, input_option, select_options, show_menu};
 
 fn main() {
+    // Process of data cleaning and import of data from "computer_games.csv" to main.rs
+    clean_data();
+    import_data();
+
+    // Main program
     loop {
         show_menu();
 
         let option = input_option();
 
-        let Ok(menu_item) = from_str(&option) else {
-            println!("This option does not exist");
-            continue;
-        };
+        let menu_item =  from_str(&option);
 
-        if select_options(&menu_item) {
-            break;
+        match select_options(&menu_item) {
+            Ok(true) => break, 
+            Ok(false) => continue, 
+            Err(msg) => println!("{}", msg),
         }
     }    
+}
+
+fn clean_data() {
+    todo!()
+}
+
+fn import_data() {
+    todo!()
 }
