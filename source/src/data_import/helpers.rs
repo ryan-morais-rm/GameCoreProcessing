663bf8a00435b1e035c3dbd65b7a88a790f2c235
Ryan de Morais Correia <ryan.morais.workspace@gmail.com>
2026-02-13 21:01:05 -0300

    New function that checks data parsing and cleaning and shows informations about it in CLI

--- source/src/data_import/helpers.rs
+++ source/src/data_import/helpers.rs
@@ -12,3 +12,20 @@ pub fn extract_game_name(line: &str) -> String {
     }
     name.trim().trim_matches('"').to_string()
 }
+
+pub fn check_dataset(content: &Vec<String>) {
+    println!("\n=== DEBUG: Checking data integrity ===");        
+    println!("--- HEAD (First 5 lines) ---");
+    
+    for line in content.iter().take(5) {
+        println!("{}", line);
+    }
+    
+    println!("\n--- TAIL (Last 5 lines) ---");
+    if content.len() > 5 {
+        for line in content.iter().skip(content.len() - 5) {
+            println!("{}", line);
+        }
+    }
+    println!("=========================================\n");
+}
